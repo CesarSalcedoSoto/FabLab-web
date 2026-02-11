9b554562f4f7768ef3243d1e305a2ae03a46a127
Cesar Salcedo <cesarsalcedo15.08@gmail.com>
2026-02-11 13:20:49 -0300

    feat: add EquipmentRequests, EquipmentUsage, and EquipmentReservations to collections

--- web/src/features/cms/infrastructure/payload/collections/index.ts
+++ web/src/features/cms/infrastructure/payload/collections/index.ts
@@ -21,6 +21,9 @@ export { Categories } from './Categories';
 // Servicios y Equipamiento
 export { Services } from './Services';
 export { Equipment } from './Equipment';
+export { EquipmentRequests } from './EquipmentRequests';
+export { EquipmentUsage } from './EquipmentUsage';
+export { EquipmentReservations } from './EquipmentReservations';
 
 // Equipo
 export { TeamMembers } from './TeamMembers';
@@ -42,6 +45,9 @@ import { Posts } from './Posts';
 import { Categories } from './Categories';
 import { Services } from './Services';
 import { Equipment } from './Equipment';
+import { EquipmentRequests } from './EquipmentRequests';
+import { EquipmentUsage } from './EquipmentUsage';
+import { EquipmentReservations } from './EquipmentReservations';
 import { TeamMembers } from './TeamMembers';
 import { Projects } from './Projects';
 import { Events } from './Events';
@@ -58,6 +64,9 @@ export const collections = [
     // Servicios
     Services,
     Equipment,
+    EquipmentRequests,
+    EquipmentUsage,
+    EquipmentReservations,
     // Equipo
     TeamMembers,
     // Proyectos
